ee73e5d47919925a9fa599394325ed92de62fb66
Fayorin <126774711+Fayorin@users.noreply.github.com>
2025-05-12 11:42:26 +0300

    Update filters.py

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,38 +1,34 @@
-from django_filters import rest_framework
-from recipes.models import Recipe, Ingredient
+from django_filters import rest_framework as filters
+from recipes.models import Dish, Product
 
 
-class RecipeFilter(rest_framework.FilterSet):
-    is_favorited = rest_framework.filters.BooleanFilter(
-        method='filter_is_favorited'
-    )
-    is_in_shopping_cart = rest_framework.filters.BooleanFilter(
-        method='filter_is_in_shopping_cart'
-    )
+class DishFilter(filters.FilterSet):
+    bookmarked = filters.BooleanFilter(method='filter_bookmarked')
+    in_basket = filters.BooleanFilter(method='filter_in_basket')
 
     class Meta:
-        model = Recipe
-        fields = ['author', 'is_favorited', 'is_in_shopping_cart']
+        model = Dish
+        fields = ['creator', 'bookmarked', 'in_basket']
 
-    def filter_is_favorited(self, queryset, name, value):
-        user = self.request.user
-        if user.is_authenticated and value:
+    def filter_bookmarked(self, queryset, name, value):
+        user = getattr(self.request, 'user', None)
+        if user and user.is_authenticated and value:
             return queryset.filter(favorited_by__user=user)
         return queryset
 
-    def filter_is_in_shopping_cart(self, queryset, name, value):
-        user = self.request.user
-        if user.is_authenticated and value:
+    def filter_in_basket(self, queryset, name, value):
+        user = getattr(self.request, 'user', None)
+        if user and user.is_authenticated and value:
             return queryset.filter(in_carts__user=user)
         return queryset
 
 
-class IngredientFilter(rest_framework.FilterSet):
-    name = rest_framework.filters.CharFilter(
-        field_name='name',
+class ProductFilter(filters.FilterSet):
+    name = filters.CharFilter(
+        field_name='title',
         lookup_expr='istartswith',
     )
 
     class Meta:
-        model = Ingredient
+        model = Product
         fields = ['name']
